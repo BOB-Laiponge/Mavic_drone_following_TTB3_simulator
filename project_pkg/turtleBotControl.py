ad67ea6144ca52ad4533f8d0e60945da916e0696
BOB_Laiponge <py.pitiot@gmail.com>
2024-01-25 10:59:04 +0100

    added ttbc

---
+++ project_pkg/turtleBotControl.py
@@ -0,0 +1,9 @@
+
+
+
+class TurtleBotControl(Node): 
+    def __init__(self):
+        # Initiate the Node class's constructor and give it a name
+        super().__init__('TurtleBotControl')
+
+        
