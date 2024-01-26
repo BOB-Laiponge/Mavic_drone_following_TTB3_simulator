0a280ad582f572e0adfb3724fb4d2e9a9527cb8f
BOB_Laiponge <py.pitiot@gmail.com>
2024-01-26 19:54:11 +0100

    wip

--- project_pkg/turtleBotControl.py
+++ project_pkg/turtleBotControl.py
@@ -9,6 +9,8 @@ from geometry_msgs.msg import PointStamped # /gps/gps
 
 from std_msgs.msg import Float32           # /gps/speed
 from nav_msgs.msg import Odometry           # /gps/speed
+from sensor_msgs.msg import Imu            # /
+
 #from rclpy.qos import qos_profile_sensor_data
 
 
@@ -17,20 +19,41 @@ class TurtleBotControl(Node):
         # Initiate the Node class's constructor and give it a name
         super().__init__('TurtleBotControl')
         self.subscription_1 = message_filters.Subscriber(self, PointStamped, '/TurtleBot3Burger/gps')
-        self.subscription_2 = message_filters.Subscriber(self, Odometry, '/odom')
-        self.subscription_3 = message_filters.Subscriber(self, Vector3, '/TurtleBot3Burger/gps/speed_vector')
-        self.publisher_cmd = self.create_publisher(Twist, '/TurtleBot3Burger/cmd_vel', 10)
+        #self.subscription_2 = message_filters.Subscriber(self, Imu, '/TurtleBot3Burger/imu')
+        #self.subscription_3 = message_filters.Subscriber(self, Vector3, '/TurtleBot3Burger/gps/speed_vector')
+        #self.publisher_cmd = self.create_publisher(Twist, '/TurtleBot3Burger/imu', 10)
 
-        self.ts = message_filters.ApproximateTimeSynchronizer([self.subscription_1, self.subscription_3], 30, 0.01, allow_headerless=True)
-        self.ts.registerCallback(self.callbackGPS)
+        #self.ts = message_filters.ApproximateTimeSynchronizer([self.subscription_1, self.subscription_2], 30, 0.01, allow_headerless=True)
+        #self.ts.registerCallback(self.callbackGPS)
 
 
-        #self.create_subscription(PointStamped, '/TurtleBot3Burger/gps', self.callbackGPS, 1)
+        #self.create_subscription(Imu, '/TurtleBot3Burger/imu', self.callbackGPS, 1)
+        self.create_subscription(Float32, '/TurtleBot3Burger/gps/speed', self.callbackSpeed, 1)
+        self.create_subscription(Imu,'/TurtleBot3Burger/imu', self.callbackImu, 1)
+        self.create_subscription(Imu,'/TurtleBot3Burger/gps', self.callbackImu, 1)
+
 
         self.next_pose=PointStamped()
+        self.last_speed = Float32()
+        self.last_imu = Imu()
 
-    def callbackGPS(self, msg, msg2):
-        x = msg.point.x
+    def callbackImu(self, imu):
+        print(f"imu {imu.angular_velocity}")
+        pass
+
+    def callbackSpeed(self, speed):
+        #print(speed.data)
+        pass
+
+    def callbackGPS(self, msg2, msg):
+        qx = msg.orientation.x
+        qy = msg.orientation.y
+        qz = msg.orientation.z
+        qw = msg.orientation.w
+
+        print(qx)
+
+        """x = msg.point.x
         y = msg.point.y
         z = msg.point.z
         # Calcul trajectoire
@@ -39,7 +62,7 @@ class TurtleBotControl(Node):
         
         kinematic = [0.3, 0.2]
         # Kinetic : position, angular
-        self.publish_results([0.3,0.0])
+        self.publish_results([0.3,0.0])"""
 
     def publish_results(self, vel):
         msg = Twist() # Create a message of this type   
